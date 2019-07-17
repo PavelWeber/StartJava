c3fb44c305ff9b19fee0fed10a8c73506030565f
pavel.tkalin@electrolux.com <pavel.tkalin@electrolux.com>
2019-07-17 09:47:08 +0300

    HW1

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,8 +1,5 @@
-public class Variable
-{
-    
-	public static void main(String [] args){
-
+public class Variable{
+	public static void main(String[] args) {
 		byte coreNumber = 2;
 		short digit = 64;
 		int  hddSizeGb = 465;
@@ -11,13 +8,14 @@ public class Variable
 		double gigaherz = 2.2d;
 		char hddMainName = 'C';
 		boolean isWorking = true;
-		System.out.println (coreNumber);
-		System.out.println (digit);
-		System.out.println (hddSizeGb);
-		System.out.println (ramGb);
-		System.out.println (hddSizeFree);
-		System.out.println (gigaherz);
-		System.out.println (hddMainName);
-		System.out.println (isWorking); }
 
+		System.out.println(coreNumber);
+		System.out.println(digit);
+		System.out.println(hddSizeGb);
+		System.out.println(ramGb);
+		System.out.println(hddSizeFree);
+		System.out.println(gigaherz);
+		System.out.println(hddMainName);
+		System.out.println(isWorking); 
+	}
 }
