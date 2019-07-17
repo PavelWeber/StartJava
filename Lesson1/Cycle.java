0ba752c9a6efde4e849f119450c37f8afb9ed958
pavel.tkalin@electrolux.com <pavel.tkalin@electrolux.com>
2019-07-17 17:35:21 +0300

    HW1

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,45 +1,22 @@
-public class Cycle
-
-{
-	
-
-public static void main (String[]args)
-
-
-{
-	
-
-
-for(int i=0; i<=20; i++)
-{
-System.out.println(i);
-}
-
-int m =6;
-
-while (m>=-6)
-
-{
-System.out.println(m);
-m=m-2;
-
-}
-
-int n = 10;
-int result=10;
-
-do 
-
-{
-n = n+2;
-result=result+n;
-
-}
-
-while (n<20);
-System.out.println(result);
-
-
-}
-
+public class Cycle{
+	public static void main (String[]args) {
+//Задание 1	
+		for(int i=0; i<=20; i++) {
+			System.out.println(i);
+		}
+	//Задание 2
+		int m =6;
+		while (m>=-6){
+			System.out.println(m);
+			m=m-2;
+		}
+	//Задание 3
+		int n = 10;
+		int result=10;
+		do {
+			n = n+2;
+			result=result+n;
+		} while (n<20);
+		System.out.println(result);
+	}
 }
