aac98e6baec154dc1666009b2b1af495e1213726
Pavel Tkalin <tkalin-pavel@yandex.ru>
2019-07-20 14:59:18 +0300

    HW1

---
+++ Lesson1/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+
+	String sex = "man";
+	String name ="John";
+	int height = 191;
+	int weight=89;
+
+	boolean 
+
+
+
+
+
+
+
+}
--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -1,22 +1,10 @@
-public class Unicode
+public class Unicode {
 
-{
-	
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-char x;
-
-for (int m=9398;m<=10178;m++)
-
-{
-
-m=1
-char x = '\u';
-System.out.println (x);
-
-}
-	
-}
-
-
+		for (int m = 33;m <= 126;m++) {
+			char unicodeSymbol='\u0039';
+			System.out.println (unicodeSymbol + m);
+		}		
+	}
 }
