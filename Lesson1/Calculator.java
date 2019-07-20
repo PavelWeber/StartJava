f4fe382e6b1e44299e5ef916cf47503941014375
Pavel Tkalin <tkalin-pavel@yandex.ru>
2019-07-20 12:48:36 +0300

    HW1

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,69 +1,44 @@
-public class Calculator
-
-{
+public class Calculator {
 	
-public static void main (String[]args)
+	public static void main (String[] args) {
+		int a = 3;
+		int b = 4;
+		int result;
+		char sign = '^';
 
-{
-
-int a = 2;
-int b = 4;
-int result;
-char sign = '^';
-
-if (sign == '/')
-
-	{
- result = a/b;
- System.out.println (result);
-	}
-else if (sign == '+')
-
-	{
- result = a+b;
- System.out.println (result);
-	}
-
-else if (sign == '-')
-
-	{
- result = a-b;
- System.out.println (result);
-	}
-
-else if (sign == '*')
-
-	{
- result = a*b;
- System.out.println (result);
-	}
-
-else if (sign == '%')
-
-	{
- result = a%b;
- System.out.println (result);
-	}
-
-else if (sign == '^')
-
-	{
-		result = 1;
-
-		for(int i=1;i<=b;i++)
-
-		{
-			result = result*a;
-			
+		if (sign == '/') {
+			result = a / b;
+			System.out.println(result);
 		}
 
+		else if (sign == '+') {
+			result = a + b;
+	    	System.out.println(result);
+		}
 
-          System.out.println (result);
+		else if (sign == '-'){
+			result = a - b;
+			System.out.println(result);
+		}
+
+		else if (sign == '*') {
+			result = a * b;
+			System.out.println(result);
+		}
+
+		else if (sign == '%') {
+			result = a % b;
+			System.out.println(result);
+		}
+
+		else if (sign == '^') {
+			result = 1;
+				for(int i = 1; i <= b; i++) {
+					result *= a;			
+				}
+				System.out.println (result);
+		}
+
 	}
-
-
-
-
-}
 
 }
