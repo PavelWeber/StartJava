5b86dbd3ebe7602c165f16d08e7afa69090b2080
Pavel Tkalin <tkalin-pavel@yandex.ru>
2019-07-20 15:37:59 +0300

    HW1

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -2,14 +2,19 @@ public class MyFirstGame {
 
 	public static void main(String[] args) {
 		int computerNumber = 67;
-		int humanNumber = 67;
+		int humanNumber = 0;
 
-		if (humanNumber < computerNumber) {
-			System.out.println ("Your figure is less than computer suggested");
-		} else if (humanNumber > computerNumber) {
-				System.out.println ("Your figure is more than computer suggested");
-		} else {
-				System.out.println ("You won!");
+		for (int i = 0; i <=100 ; i++){
+			if (humanNumber < computerNumber) {
+				System.out.println("Your figure is less than computer suggested");
+				humanNumber++;
+			} else if (humanNumber > computerNumber) {
+					System.out.println("Your figure is more than computer suggested");
+					humanNumber--;
+			} else {
+					System.out.println("You won!");
+					break;
+			}
 		}
 	}
 }
--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -2,9 +2,9 @@ public class Unicode {
 
 	public static void main(String[] args) {
 
-		for (int m = 33;m <= 126;m++) {
+		for (int m = 33; m <= 126; m++) {
 			char unicodeSymbol='\u0039';
-			System.out.println (unicodeSymbol + m);
+			System.out.println(unicodeSymbol + m);
 		}		
 	}
 }
