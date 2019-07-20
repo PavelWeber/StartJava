066cae5409d78cc84f79603dd560cdb69ec84b0f
Pavel Tkalin <tkalin-pavel@yandex.ru>
2019-07-20 16:46:14 +0300

    HW1

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,20 +1,17 @@
 public class MyFirstGame {
-
-	public static void main(String[] args) {
-		int computerNumber = 67;
-		int humanNumber = 0;
-
-		for (int i = 0; i <= 99; i++){
-		if (humanNumber < computerNumber) {
-			System.out.println("Your figure is less than computer suggested");
-			humanNumber++;
-		} else if (humanNumber > computerNumber) {
+    public static void main(String[] args) {
+        int computerNumber = 67;
+        int humanNumber = 0;
+		
+        while (humanNumber != computerNumber) {
+			if (humanNumber < computerNumber) {
+				System.out.println("Your figure is less than computer suggested");
+				humanNumber++;
+			} else if (humanNumber > computerNumber) {
 				System.out.println("Your figure is more than computer suggested");
 				humanNumber--;
-		} else {
-				System.out.println("You won!");
-				break;
-			}
-		}
-	}
+			} 
+        }        
+        System.out.println("You won!");                   
+    }
 }
--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -1,10 +1,7 @@
 public class Unicode {
-
 	public static void main(String[] args) {
-
-		for (int m = 33; m <= 126; m++) {
-			char unicodeSymbol='U+0039';
-			System.out.println(unicodeSymbol + m);
+		for (char i = 33; i <= 126; i++) {
+			System.out.println(i);
 		}		
 	}
 }
