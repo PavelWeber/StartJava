3a4645547e8b6284f2039c2dd43aae767427cf7e
pavel.tkalin@electrolux.com <pavel.tkalin@electrolux.com>
2019-07-25 16:44:45 +0300

    HW2

---
+++ Lesson2/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+	String sex = "man";
+	String name ="John";
+	int height = 191;
+	int weight=89;
+
+	void go(){
+	}
+	void sit(){
+	}
+	void run(){		
+	}
+	void talk(){		
+	}
+	void learnJava(){		
+	}
+}
---
+++ Lesson2/Wolf.java
@@ -0,0 +1,49 @@
+public class Wolf{
+	private String sex = "man";
+	private String name ="Volk";
+	private int age = 5;
+	private String colour="red";
+	public String getWolfSex(){
+		return sex;
+	}
+	public String getWolfName(){
+		return name;
+	}
+	public int getWolfAge(){
+		return age;
+	}
+	public String getWolfColour(){
+		return colour;
+	}
+	public void setWolfSex(String sex){
+		this.sex=sex;
+	}
+	public void setWolfName(String name){
+		this.name=name;
+	}
+	public void setWolfAge(int age){
+		if(age>8){
+			System.out.println("age is incorrect");
+		} else{
+			this.age=age;
+		}
+	}
+	public void setWolfColour(String colour){
+		this.colour=colour;
+	}
+	public void go(){
+		System.out.println("Wolf goes");
+	}
+	public void sit(){
+		System.out.println("Wolf sits");
+	}
+	public void run(){
+		System.out.println("Wolf runs");		
+	}
+	public void vow(){
+		System.out.println("Wolf vows");		
+	}
+	public void hunt(){
+		System.out.println("Wolf hunts");		
+	}
+}
