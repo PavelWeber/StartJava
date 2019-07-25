393172d9454ad9fee9b1357583dcff894d5f279f
pavel.tkalin@electrolux.com <pavel.tkalin@electrolux.com>
2019-07-25 17:42:36 +0300

    HW2

---
+++ Lesson1/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+	String sex = "man";
+	String name = "John";
+	int height = 191;
+	int weight = 89;
+
+	void go() {
+	}
+
+	void sit() {
+	}
+
+	void run() {		
+	}
+
+	void talk() {		
+	}
+
+	void learnJava() {		
+	}
+
+}
