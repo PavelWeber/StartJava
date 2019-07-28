d4f47e45765ebb554e8b3f39e856e00debf259c4
Pavel Tkalin <tkalin-pavel@yandex.ru>
2019-07-28 16:47:15 +0300

    HW2

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -11,30 +11,36 @@ public class Calculator {
 	public void setB(int b) {
 		this.b=b;
 	}
-	public void sign1(char sign1) {
+	public void setSign1(char sign1) {
 		this.sign1=sign1;
 	}
-	public String sign(String sign) {
+	public void setSign(String sign) {
 		this.sign=sign;
 	}
 
 	public void count() {
 		switch (sign1) {
-			case '+': result = a + b;
-			break;
-			case '-': System.out.println(a - b);
-			break;
-			case '*': System.out.println(a * b);
-			break;
-			case '/': System.out.println(a / b);
-			break;
-			case '^': int result = 1;
-			for(int i = 1; i <= b; i++) {
-					result *= a;			
-				}
-		
-			break;}
+			case '+':
+				System.out.println(a + b);
+				break;
+			case '-':
+				System.out.println(a - b);
+				break;
+			case '*':
+				System.out.println(a * b);
+				break;
+			case '/':
+				System.out.println(a / b);
+				break;
+			case '%':
+				System.out.println(a % b);
+				break;
+			case '^':
+				int result = 1;
+				for(int i = 1; i <= b; i++) {
+						result *= a;			
+					}
+				System.out.println(result);
+				break;}
 		}
-	
-
 }
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-public class CalculatorTest{
+public class CalculatorTest {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Calculator calculator1 = new Calculator();
 
 		System.out.println("Please input an integer ");
 		int a = scan.nextInt();
+		calculator1.setA(a);
 		System.out.println("Please input an operation sign ");
 		String sign = scan.next();
 		char sign1 = sign.charAt(0);
+		calculator1.setSign1(sign1);
 		System.out.print("Please input an integer ");
 		int b = scan.nextInt();
+		calculator1.setB(b);
 
 		calculator1.count();
-
 	}
 }
 
