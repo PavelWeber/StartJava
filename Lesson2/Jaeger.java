b82eda1ca37f68bc4c1907b4ca5538452e398504
Pavel Tkalin <pavel.tkalin@electrolux.com>
2019-07-29 13:27:49 +0300

    HW1

--- Lesson2/Jaeger.java
+++ Lesson2/Jaeger.java
@@ -10,45 +10,45 @@ public class Jaeger {
 
 	public void setModelName(String modelName) {
 		this.modelName = modelName;
-		}
+	}
 
 	public void setMark(String mark) {
 		this.mark = mark;
-		}
+	}
 
 	public void setOrigin(String origin) {
 		this.origin = origin;
-		}
+	}
 
 	public void setHeight(float height) {
 		this.height = height;
-		}
+	}
 
 	public void setWeight(float weight) {
 		this.weight = weight;
-		}
+	}
 
 	public void setSpeed(int speed) {
 		this.speed = speed;
-		}
+	}
 
 	public void setStrength(int strength) {
 		this.strength = strength;
-		}
+	}
 
 	public void setArmor(int armor){
 		this.armor = armor;
-		}
+	}
 
 	public void move() {
 		System.out.println("Robot is moving");
-		}
+	}
 
 	public void scanKaiju() {
 		System.out.println("Robot is scanning");
-		}
+	}
 
 	public void useVortexCannon() {
 		System.out.println("Robot is shooting");
-		}
+	}
 }
