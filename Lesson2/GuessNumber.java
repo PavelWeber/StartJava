76443fd8cd95700c5ac46eb24c409c31575a4dd6
pavel.tkalin@electrolux.com <pavel.tkalin@electrolux.com>
2019-08-12 14:05:11 +0300

    HW2

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -3,11 +3,10 @@ import java.lang.Math;
 
 public class GuessNumber {
 
-    int computerNumber = (int)(Math.random() * 101);
+    private int computerNumber = (int)(Math.random() * 101);;
     private Player player1;
     private Player player2;
-    Scanner scan = new Scanner(System.in);
-    private boolean interval;
+    private Scanner scan = new Scanner(System.in);
 
     public  GuessNumber(Player player1, Player player2){
         this.player1 = player1;
@@ -15,14 +14,12 @@ public class GuessNumber {
     }
 
     public void play() {
-
-        while((player1.getNumber() != computerNumber) | (player2.getNumber() != computerNumber) | (interval == true)) {
+       for(int i=0; ;i++) {
             System.out.println("Insert first player's number");
             player1.setNumber(scan.nextInt());
 
             if(player1.getNumber() > 100 | player1.getNumber() < 0) {
                 System.out.println(player1.getName() + " have input number out of the interval. Game over");
-                interval = false;
                 break;
             }
             if (player1.getNumber() == computerNumber) {
@@ -36,7 +33,6 @@ public class GuessNumber {
 
             if(player2.getNumber() > 100 | player2.getNumber() < 0) {
                 System.out.println(player2.getName() + " have input number out of the interval. Game over");
-                interval = false;
                 break;
             }
             if (player2.getNumber() == computerNumber) {
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
-
 public class GuessNumberTest {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Insert first player's name");
-        Player player1 = new Player(scan.nextLine());
-        System.out.println("Insert second player's name");
-        Player player2 = new Player(scan.nextLine());
-        GuessNumber game = new GuessNumber(player1,player2);
-        game.play();
-    }
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String answer = "yes";	 	
+	 	while (answer.equals("yes")) {
+        	System.out.println("Insert first player's name");
+	        Player player1 = new Player(scan.nextLine());
+	        System.out.println("Insert second player's name");
+	        Player player2 = new Player(scan.nextLine());
+	        GuessNumber game = new GuessNumber(player1,player2);
+	        game.play();
+			
+			do {
+				System.out.println("do you want to continue yes/no");
+				answer = scan.nextLine();
+			} while (!answer.equals("yes") && !answer.equals("no"));				 		
+	 	}		
+	}
 }
+
+
+
