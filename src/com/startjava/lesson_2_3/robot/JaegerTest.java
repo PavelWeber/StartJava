1fd02db5747bce7eb630777a77451287dcdd39a2
pavel.tkalin@electrolux.com <pavel.tkalin@electrolux.com>
2019-08-15 15:43:47 +0300

    HW3

--- src/com/startjava/lesson_2_3/robot/JaegerTest.java
+++ src/com/startjava/lesson_2_3/robot/JaegerTest.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.robot;
+
 public class JaegerTest {
 	public static void main(String[] args) {
 		Jaeger gipsy = new Jaeger("Gipsy", "Mark2", "USA", 98, 1998, 7, 9, 1000);
