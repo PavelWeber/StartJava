c6935a3b6ee015e69512e3ad8b43e81ca2f1396f
pavel.tkalin@electrolux.com <pavel.tkalin@electrolux.com>
2019-08-27 17:43:13 +0300

    HW4

---
+++ src/com/startjava/lesson_4/intro/ArrayStorage.java
@@ -0,0 +1,58 @@
+/**
+ * Array based storage for Resumes
+ */
+package com.startjava.lesson_4.intro;
+
+import java.util.Arrays;
+
+
+public class ArrayStorage {
+
+    private int counter = 0;
+
+    Resume[] storage = new Resume[10];
+
+    void clear() {
+        Arrays.fill(storage, null);
+    }
+
+    void save(Resume r) {
+        storage[counter] = r;
+        counter++;
+    }
+
+    Resume get(String uuid) {
+            for (Resume r: storage) {
+                if (r.uuid == uuid) {
+                    return r;
+                }
+            }
+       return null;
+    }
+
+
+    void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = null;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        return Arrays.copyOf(storage, storage.length);
+    }
+
+    int size() {
+        int j = 0;
+        for (Resume r : storage) {
+            if (r != null) {
+                j++;
+            }
+        }
+        return j;
+    }
+}
---
+++ src/com/startjava/lesson_4/intro/Resume.java
@@ -0,0 +1,15 @@
+package com.startjava.lesson_4.intro;
+
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    // Unique identifier
+    String uuid;
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
